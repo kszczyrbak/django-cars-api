79f6aa3f4443b1e0db52878376fcf773f4275fca
Chris <kszczyrbak8@gmail.com>
2021-02-14 10:42:26 +0100

    changed static files conf

--- django_cars/settings.py
+++ django_cars/settings.py
@@ -11,9 +11,6 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-from dotenv import load_dotenv
-
-load_dotenv()
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -100,6 +97,8 @@ DATABASES = {
         'PORT': os.environ['POSTGRES_PORT']
     }
 }
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 
 # Password validation
